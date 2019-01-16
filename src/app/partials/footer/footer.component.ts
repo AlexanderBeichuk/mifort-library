d2a606d162d3f2fa0cd3482c06f06351480cbe05
KirylTytsyk <kiryltytsyk@gmail.com>
2019-01-16 18:36:58 +0300

    change login page

--- src/app/partials/footer/footer.component.ts
+++ src/app/partials/footer/footer.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {BOOKS} from '../../models/mock';
 
 @Component({
   selector: 'app-footer',
@@ -7,8 +6,6 @@ import {BOOKS} from '../../models/mock';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
-  public previewBooks = BOOKS;
 
   constructor() { }
 
