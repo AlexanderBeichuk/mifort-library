3540cf0a3db87ac690458d0ced98038d451f0c2c
AlexanderBeichuk <alexander.beichuk@gmail.com>
2019-01-17 10:47:19 +0300

    Init book table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { MDBBootstrapModule, TableModule } from 'angular-bootstrap-md';
 import {SnotifyModule, SnotifyService, ToastDefaults} from 'ng-snotify';
 import { AppRoutingModule } from './routing/app-routing.module';
 
 import { AppComponent } from './app.component';
 import {AuthorizationModule} from './authorization/authorization.module';
+import { BooksTableComponent } from './partials/books-table/books-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     MDBBootstrapModule.forRoot(),
+    TableModule,
     SnotifyModule,
     AuthorizationModule
   ],
